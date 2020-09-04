420c0f01ff5a82d5b7d946b1af6bd5625df765e5
maurya-ankit <ankitmaurya18@cse.iiitp.ac.in>
2020-09-04 21:35:36 +0530

    for heroku

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import Home,index,short
 urlpatterns = [
     path('short',short.as_view()),
-    #path('<str:token>',Home),
+    path('<str:token>',Home),
     path('',index,name='index'),
 
 ]
--- redirect/settings.py
+++ redirect/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
